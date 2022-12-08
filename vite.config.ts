38d5a324e9215335793eea930b0f5a7d19d13618
jarcadiobuendia <jarcadiobuendia26@gmail.com>
2022-12-08 15:42:59 +0300

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+process.env.BROWSER = 'chrome';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
   plugins: [react()],
   build: {
     rollupOptions: {
